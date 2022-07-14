1645f7fbfdfdbce6b2b2239beab0a2e3e0e4702f
Michael Champlin <mchamplin@sbcglobal.net>
2022-07-13 21:17:40 -0700

    Add DB op's into

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,5 @@
 from flask import (Blueprint, render_template, request, redirect)
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
@@ -10,7 +11,17 @@ def fact_page():
 @bp.route('/', methods=['GET', 'POST'])
 def index():
     if (request.method == "POST"):
-        print(request.form)
+        print(request)
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
         return redirect("/facts")
 
-    return render_template("facts/index.html")
+    # Do in case of GET or POST
+    results = models.Fact.query.all()
+
+    return render_template("facts/index.html", facts=results)
