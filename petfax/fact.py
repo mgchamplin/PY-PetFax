7c82ae3608652dd7770d5c9ea9009f74eab9c5c2
Michael Champlin <mchamplin@sbcglobal.net>
2022-07-13 19:36:00 -0700

    Added Facts index page, redirect, GET, POST

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, request, redirect)
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
@@ -6,3 +6,11 @@ bp = Blueprint('fact', __name__, url_prefix="/facts")
 def fact_page():
     print("ADD A FACT ") 
     return render_template("facts.html") 
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if (request.method == "POST"):
+        print(request.form)
+        return redirect("/facts")
+
+    return render_template("facts/index.html")
